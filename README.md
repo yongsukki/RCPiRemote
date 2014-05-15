b7e19d31a55e7b67164f90a5324536d146b97f57
Seungrok Han <muster22@naver.com>
2014-05-15 17:17:11 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+RCPiRemote
+=========
+
+Raspberrypi RC-Car for Android App.
+
+Project by www.rasplay.org - Multi-Control-RCCar
+
+
+Dependency
+HW 
+ 1. RC Car with DC Motor
+ 2. Raspberry-pi, it is ultra-low-cost ($35) credit-card sized computer, can run Linux.
+ 3. (optional) Multipi
+SW
+ 1. Android Application in Google Play
+    RC Pi Remote (https://play.google.com/store/apps/details?id=org.rasplay.rcpiremote)
+
+RCPiRemote Clone
+$ git
+
+and, run
+$ sudo python joystick.py
+
+Enjoy!!  
---
+++ setup.sh
@@ -0,0 +1,30 @@
+#RCPiRemote Installation Scripts
+# by fendergold@gmail.com , www.rasplay.org
+
+# Install WebIOPi
+cd ~
+wget http://downloads.sourceforge.net/project/webiopi/WebIOPi-0.7.0.tar.gz
+tar xvf WebIOPi-0.7.0.tar.gz
+cd WebIOPi-0.7.0
+sudo ./setup.sh
+# WebIOPi Start
+sudo /etc/init.d/webiopi start
+# Auto Start WebIOPi at Raspberrypi startup
+sudo update-rc.d webiopi defaults
+
+
+# mjpeg with mmal Drive
+# Install Defendencies
+sudo apt-get install git cmake libjpeg8-dev imagemagick -y
+sudo ln -s /usr/include/linux/videodev2.h /usr/include/linux/videodev.h
+# Install mjpg-streamer
+cd ~
+git clone https://github.com/liamfraser/mjpg-streamer
+cd ~/mjpg-streamer/mjpg-streamer-experimental
+make clean all
+# Run mjpg-streamer
+echo 'STREAMER_PATH=/home/pi/mjpg-streamer/mjpg-streamer-experimental' >> ~/runmjpgstream.sh
+echo 'export LD_LIBRARY_PATH=$STREAMER_PATH' >> ~/runmjpgstream.sh
+echo '$STREAMER_PATH/mjpg_streamer -i "input_raspicam.so -d 200 -rot 180" -o "output_http.so -w $STREAMER_PATH/www -p 8080" &' >> ~/runmjpgstream.sh
+sh ~/runmjpgstream.sh
+
